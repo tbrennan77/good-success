95f5f3f8fc170cbc0f9e3382a1135474655b8b59
Timothy Brennan <2timothy215@gmail.com>
2019-04-08 10:06:21 -0400

    Update Soil

--- lib/setup.php
+++ lib/setup.php
@@ -13,8 +13,11 @@ function setup() {
   add_theme_support('soil-clean-up');
   add_theme_support('soil-nav-walker');
   add_theme_support('soil-nice-search');
+  add_theme_support('soil-google-analytics', 'UA-121814744-1');
   add_theme_support('soil-jquery-cdn');
   add_theme_support('soil-relative-urls');
+  add_theme_support('soil-js-to-footer');
+  add_theme_support('soil-disable-rest-api');
 
   // Make theme available for translation
   // Community translations can be found at https://github.com/roots/sage-translations
