5d8fb8fc2e6bfdd3b42383ef444c61f8f9d03d59
Timothy Brennan <2timothy215@gmail.com>
2019-04-08 10:49:29 -0400

    Remove Bower. Update NPM, Gulp and Composer. Added Font-Awesome Pro and removed BX-Slider

--- gulpfile.js
+++ gulpfile.js
@@ -253,7 +253,7 @@ gulp.task('clean', require('del').bind(null, [path.dist]));
 gulp.task('watch', function() {
   browserSync.init({
     files: ['{lib,templates}/**/*.php', '*.php'],
-    proxy: config.devUrl,
+    proxy: 'http://localhost:8888/',
     snippetOptions: {
       whitelist: ['/wp-admin/admin-ajax.php'],
       blacklist: ['/wp-admin/**']
