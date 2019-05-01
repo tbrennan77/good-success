1e6d8e62f882ae766f16118390051adb7e7a1e38
Timothy Brennan <2timothy215@gmail.com>
2019-05-01 12:52:31 -0400

    Mobile menu fix. Category slug fix

--- templates/head.php
+++ templates/head.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=false;">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta content="index, follow" name="robots">
   <meta content="INDEX, FOLLOW" name="GOOGLEBOT">
   <meta name="robots" content="ALL" >
